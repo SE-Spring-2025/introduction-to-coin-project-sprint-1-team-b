6b860888ab124f131b1d6b610058d3695e51691f
J-Loftis85 <jadenloftis85@gmail.com>
2025-03-21 12:51:36 -0400

    Added method framework for Coin class

--- Coin/src/Coin.java
+++ Coin/src/Coin.java
@@ -1,4 +1,25 @@
 public class Coin
 {
-    
+    public Coin()
+    {
+        this.value = null;
+        this.year = null
+    }
+
+    public Coin(double value)
+    {
+        this.value = value;
+        this.year = null;
+    }
+
+    public Coin(double value, int year)
+    {
+        this.value = value;
+        this.year = year;
+    }
+
+    public String toString()
+    {
+        return ":)";
+    }
 }
