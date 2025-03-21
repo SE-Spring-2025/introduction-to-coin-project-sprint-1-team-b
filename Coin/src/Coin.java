07c9a2f969008d138e0609638101afb9846c0df5
moll-e-watson <watsonme3@appstate.edu>
2025-03-21 12:49:03 -0400

    Coin Variables

--- Coin/src/Coin.java
+++ Coin/src/Coin.java
@@ -1,4 +1,15 @@
 public class Coin
 {
-    
+    double value;
+    String commonName;
+    String frontMotto;
+    int manufactureYear;
+    String frontImage;
+    String backImage;
+    String backMotto;
+    String frontLabel;
+    String backLabel;
+    String valueDescription;
+    boolean ridgedEdge;
+    String metallurgy;
 }
