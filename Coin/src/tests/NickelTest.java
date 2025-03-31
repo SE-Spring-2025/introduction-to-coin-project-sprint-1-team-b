b114d2fd5c6b3a40c13d84c5ea50c4c05666426c
moll-e-watson <watsonme3@appstate.edu>
2025-03-31 09:53:50 -0400

    Separated into folders, added Tests, modified Coin and Penny

--- Coin/src/tests/NickelTest.java
+++ Coin/src/tests/NickelTest.java
@@ -1,3 +1,25 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class NickelTest {
-    
+ @Test
+ public void testConstructors(){
+    //Test no arg Constructor
+    try {
+        Nickel d = new Nickel();
+    }
+    catch(Exception e) {
+        fail("Nickel() constructor caused exception: "+e.getMessage());
+    }
+    //test arg constructor
+    try {
+        Nickel d = new Nickel(2000);
+    } catch(Exception e) {
+        fail("Nickel() constructor caused exception: "+e.getMessage());
+    }
+
+    //no fail
+    assertTrue(true);
+ } 
 }
